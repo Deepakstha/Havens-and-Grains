27fc802dd47b7fa983a15f12266e1d33caf99866
prabinzz <prabinparajuli92@gmail.com>
2024-02-12 14:46:43 +0545

    Page not found

--- havens-and-grains-client/src/App.jsx
+++ havens-and-grains-client/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./layout";
 import Home from "./pages/Home";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
       </Route>
+      <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
 }
---
+++ havens-and-grains-client/src/pages/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PageNotFound = () => {
+  return (
+    <div className="text-center w-screen h-screen flex items-center justify-center">
+      <div className="text-6xl text-primary">404</div>
+      <div className="text-muted mt-8">Page not found</div>
+    </div>
+  );
+};
+
+export default PageNotFound;
